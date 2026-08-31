6f65938310811c76e2508dcdb334cc29aca29a5f
AlmeidaGallonte <jamersonalmeida4567@gmail.com>
2026-08-31 10:02:03 -0300

    desafio 40

---
+++ desafios/desafio040/classes.py
@@ -0,0 +1,15 @@
+class Aluno():
+    def __init__(self, nome, curso, serie):
+        pass
+
+class Usuario():
+    def __init__(self, nome, email):
+        pass
+
+class JSON():
+    def exportar():
+        pass
+
+class XML():
+    def exportar():
+            pass
